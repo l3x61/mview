0ab0134c74fcc2520c9d89644977d341006c7d8a
alex <me@l3x61.dev>
2025-05-18 01:55:58 +0200

    logging

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,14 @@
 const std = @import("std");
+const log = std.log.scoped(.main);
+
+pub const std_options = std.Options{
+    .log_level = .debug,
+    .logFn = @import("utils/logFn.zig").logFn,
+};
 
 pub fn main() !void {
+    log.debug("{s}() started", .{@src().fn_name});
+    defer log.debug("{s}() exited", .{@src().fn_name});
+
     std.debug.print("Hello World!\n", .{});
 }
---
+++ src/utils/logFn.zig
@@ -0,0 +1,24 @@
+const std = @import("std");
+const Ansi = @import("Ansi.zig");
+
+pub fn logFn(
+    comptime level: std.log.Level,
+    comptime scope: @Type(.enum_literal),
+    comptime format: []const u8,
+    args: anytype,
+) void {
+    const level_prefix = switch (level) {
+        .debug => Ansi.Dim,
+        .info => Ansi.Cyan,
+        .warn => Ansi.Yellow,
+        .err => Ansi.Red,
+    };
+    const scope_prefix = @tagName(scope);
+    const prefix = level_prefix ++ scope_prefix ++ ".";
+
+    std.debug.lockStdErr();
+    defer std.debug.unlockStdErr();
+
+    const stderr = std.io.getStdErr().writer();
+    nosuspend stderr.print(prefix ++ format ++ "\n" ++ Ansi.Reset, args) catch return;
+}
